74c13b32878b7414797ba6625e2a8500bc5c106e
Steven Zych <stevenzych@Stevens-MacBook-Pro.local>
2021-05-12 22:01:50 -0600

    Adding completed routes to precipitation and station

--- app.py
+++ app.py
@@ -33,11 +33,43 @@ def home():
     )
 
 @app.route("/api/v1.0/precipitation")
+def precip():
+    session = Session(engine)
+
+    results = session.query(measurement.date, measurement.prcp).\
+        filter(measurement.date >= "2016-08-23").all()
+    
+    session.close()
+
+    prcp_date = []
+    for date, prcp in results:
+        prcp_date_dict = {}
+        prcp_date_dict[date] = prcp
+        prcp_date.append(prcp_date_dict)
+
+    return jasonify(prcp_date)
 
 @app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    results = session.query(station.station, station.name).all()
+
+    session.close()
+
+    stations = []
+    for station, name in results:
+        station_dict = {}
+        station_dict[station] = name
+        stations.append(station_dict)
+
+    return jasonify(stations)
 
 @app.route("/api/v1.0/tobs")
 
 @app.route("/api/v1.0/<start>")
 
 @app.route("/api/v1.0/<start>/<end>")
+
+if __name__ == '__main__':
+    app.run(debug=True)
