fe0203c1f62455609460400a2230a2a283e2b8cb
Steven Zych <stevenzych@Stevens-MacBook-Pro.local>
2021-05-15 17:22:20 -0600

    Final updates

--- app.py
+++ app.py
@@ -66,10 +66,59 @@ def stations():
     return jasonify(stations)
 
 @app.route("/api/v1.0/tobs")
+def active():
+    session = Session(engine)
+
+    mostActive = session.query(measurement.date, measurement.tobs)\
+        .filter(measurement.station == 'USC00519281')\
+        .filter(measurement.date >= "2016-08-23").order_by(measurement.date).all()
+    
+    session.close()
+
+    active_station = []
+    for date, tobs in mostActive:
+        active_dict = {}
+        active_dict["Date"] = date
+        active_dict["Temperature"] = tobs
+        active_station.append(active_dict)
+
+    return jasonify(mostActive)
 
 @app.route("/api/v1.0/<start>")
+def startDate(start):
+    session = Session(engine)
+
+    givenStart = session.query(measurement.date, func.min(measurement.tobs),\
+        func.avg(measurement.tobs),func.max(measurement.tobs)\
+        .filter(measurement.date == start)).all()
+
+    session.close()
+
+    results = {"Date": givenStart[0][0],"Minimum Temperature": givenStart[0][1],\
+        "Average Temperature": givenStart[0][2],"Maximum Temperature": givenStart[0][3]}
+
+    return jasonify(results)
 
 @app.route("/api/v1.0/<start>/<end>")
+def range(start,end):
+    session = Session(engine)
+
+    sel = [func.min(measurement.tobs),
+        func.avg(measurement.tobs),
+        func.max(measurement.tobs)]
+    givenRange = session.query(*sel).filter(measurement.date >= "2015-01-01").filter(measurement.date <= "2017-01-01").all()
+
+    session.close()
+
+    all_range = []
+    for min, avg, max in givenRange:
+    range_dict = {}
+    range_dict["Minimum Temperature"] = min
+    range_dict["Average Temperature"] = avg
+    range_dict["Maximum Temperature"] = max
+    all_range.append(range_dict)
+
+    return jasonify(all_range)
 
 if __name__ == '__main__':
     app.run(debug=True)
